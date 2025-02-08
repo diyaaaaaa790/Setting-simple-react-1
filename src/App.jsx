635952a43b7e11e7d076b4403d30bda2507db908
DIYA <diya94816@gmail.com>
2025-02-08 12:06:48 +0530

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to My React App</h1>
+      <h2>Name: Diya</h2>
+      <p>Contact: diya94816@gmail.com</p>
+    </div>
+  );
+};
+
+export default App;
+
